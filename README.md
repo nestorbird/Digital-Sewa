136d6175c65f7da1f0e3812436cf02542f5e082a
Ajay <ajay.kumar@nestorbird.com>
2023-08-31 13:04:55 +0530

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Digital Sewa
+
+This app provides extended ticketing solution on ERPNext or any other App build on Frappe.
+
+#### License
+
+MIT
---
+++ digital_sewa/config/docs.py
@@ -0,0 +1,11 @@
+"""
+Configuration for docs
+"""
+
+# source_link = "https://github.com/[org_name]/digital_sewa"
+# docs_base_url = "https://[org_name].github.io/digital_sewa"
+# headline = "App that does everything"
+# sub_heading = "Yes, you got that right the first time, everything"
+
+def get_context(context):
+	context.brand_html = "Digital Sewa"
